1ff05a0e723a200dbee0ef4d9cf090e921d56093
Gralber Gomes <gralbergs@gmail.com>
2023-05-07 01:18:00 -0300

    alterações, logo add e abas

--- src/styles.js
+++
@@ -1,8 +0,0 @@
-import { createGlobalStyle } from 'styled-components';
-
-export const GlobalStyles = createGlobalStyle`
-  body {
-    background-color: ${props => (props.darkMode ? '#222' : '#fff')};
-    color: ${props => (props.darkMode ? '#fff' : '#222')};
-  }
-`;
